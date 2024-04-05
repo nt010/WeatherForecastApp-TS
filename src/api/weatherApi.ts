fb9f8311f1f792895644c173549bdc96278527c6
nt <tomnkmr0120@gmail.com>
2024-04-05 19:31:35 +0900

    地域の追加

--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -6,6 +6,7 @@ const BASE_URL = 'https://www.jma.go.jp/bosai/forecast/data/overview_forecast/';
 export const fetchWeatherData = async (areaCode: string): Promise<WeatherData | null> => {
   try {
     const response = await axios.get<WeatherData>(`${BASE_URL}${areaCode}.json`);
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error('Error fetching weather data:', error);
--- src/components/forecast.tsx
+++ src/components/forecast.tsx
@@ -23,8 +23,15 @@ const Forecast = () => {
     <div>
       <h1>天気予報</h1>
       <select value={areaCode} onChange={handleAreaChange}>
+        <option value="014100">北海道(釧路・根室地方)</option>
+        <option value="040000">宮城</option>
         <option value="130000">東京</option>
+        <option value="170000">石川</option>
+        <option value="230000">愛知</option>
         <option value="270000">大阪</option>
+        <option value="340000">広島</option>
+        <option value="400000">福岡</option>
+        <option value="471000">沖縄(本島)</option>
       </select>
       {weatherData && (
         <div>
