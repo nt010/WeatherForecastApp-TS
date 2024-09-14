ec2ec985365e0543a4e32f1d14ccaab58a25c788
nt <tomnkmr0120@gmail.com>
2024-09-15 01:35:29 +0900

    UIと日時の形式変更

--- src/components/forecast.tsx
+++ src/components/forecast.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { fetchWeatherData } from '../api/weatherApi';
 import { WeatherData } from '../types/weather';
-import styled from "styled-components";
+import { Box, Card, CardContent, Typography, MenuItem, Select, FormControl, InputLabel, SelectChangeEvent } from '@mui/material';
+
 
 const Forecast = () => {
   const [areaCode, setAreaCode] = useState('130000');
@@ -16,52 +17,55 @@ const Forecast = () => {
     fetchData();
   }, [areaCode]);
 
-  const handleAreaChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setAreaCode(e.target.value);
+  const handleAreaChange = (e: SelectChangeEvent<string>) => {
+    setAreaCode(e.target.value as string);
+  };
+
+  // 日付を O月*日（O曜日）形式に変換する関数
+  const formatDate = (datetime: string) => {
+    const date = new Date(datetime);
+    const daysOfWeek = ['日', '月', '火', '水', '木', '金', '土'];
+    return `${date.getMonth() + 1}月${date.getDate()}日(${daysOfWeek[date.getDay()]}) ${date.getHours()}時${date.getMinutes()}分`;
   };
 
   return (
-    <Wrapper>
-      <select value={areaCode} onChange={handleAreaChange}>
-        <option value="014100">北海道(釧路・根室地方)</option>
-        <option value="040000">宮城</option>
-        <option value="130000">東京</option>
-        <option value="170000">石川</option>
-        <option value="230000">愛知</option>
-        <option value="270000">大阪</option>
-        <option value="340000">広島</option>
-        <option value="400000">福岡</option>
-        <option value="471000">沖縄(本島)</option>
-      </select>
-      {weatherData && (
-        <>
-          <StyledTime>{weatherData.reportDatetime}</StyledTime>
-          <StyledText>{weatherData.text}</StyledText>
-        </>
-      )}
-    </Wrapper>
+    <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 4 }}>
+      <Card sx={{ maxWidth: 500, padding: 2 }}>
+        <CardContent>
+          <FormControl fullWidth>
+            <InputLabel id="area-select-label">地域</InputLabel>
+            <Select
+              labelId="area-select-label"
+              value={areaCode}
+              onChange={handleAreaChange}
+              label="地域"
+            >
+              <MenuItem value="014100">北海道(釧路・根室地方)</MenuItem>
+              <MenuItem value="040000">宮城</MenuItem>
+              <MenuItem value="130000">東京</MenuItem>
+              <MenuItem value="170000">石川</MenuItem>
+              <MenuItem value="230000">愛知</MenuItem>
+              <MenuItem value="270000">大阪</MenuItem>
+              <MenuItem value="340000">広島</MenuItem>
+              <MenuItem value="400000">福岡</MenuItem>
+              <MenuItem value="471000">沖縄(本島)</MenuItem>
+            </Select>
+          </FormControl>
+
+          {weatherData && (
+            <>
+              <Typography variant="h5" component="div" sx={{ marginTop: 3, fontWeight: 'bold' }}>
+                {formatDate(weatherData.reportDatetime)}
+              </Typography>
+              <Typography variant="body1" sx={{ marginTop: 2 }}>
+                {weatherData.text}
+              </Typography>
+            </>
+          )}
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
-
-const Wrapper = styled.div`
-  background-color: blue;
-  text-align: center;
-  line-height: 60px;
-  margin: 30px;
-`;
-const StyledTime = styled.div`
-  background-color: blue;
-  text-align: center;
-  line-height: 60px;
-  margin: 30px;
-  color: white;
-  font-size: 30px;
-  font-weight: bold;
-`;
-const StyledText = styled.div`
-  line-height: 60px;
-  margin: 30px;
-  color: white;
-`;
 
 export default Forecast;
--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import styled from "styled-components";
+import WbSunnyIcon from '@mui/icons-material/WbSunny'; // 天気アイコンとして使用
 
 export const Title = () => {
   return (
     <Wrapper>
-      <StyledTitle>天気予報を表示します</StyledTitle>
+      <StyledTitle>
+        <WbSunnyIcon style={{ fontSize: 40, marginRight: 10 }} />
+        Weather Forecast
+      </StyledTitle>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
-  background-color: orange;
-  height: 60px;
-  margin: 30px;
+  background: linear-gradient(45deg, #2196F3, #21CBF3); /* グラデーションを追加 */
+  height: 80px;
+  margin: 20px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
 `;
 
 const StyledTitle = styled.div`
   color: white;
-  font-size: 30px;
+  font-size: 36px;
   font-weight: bold;
-  text-align: center;
-  line-height: 60px;
+  font-family: 'Roboto', sans-serif; /* フォントスタイルを変更 */
+  display: flex;
+  align-items: center;
 `;
+
+export default Title;
